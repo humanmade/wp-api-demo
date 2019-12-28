6e69dace4020cdf5ba99452d4f0c20a11ffe27e8
Joe Hoyle <joehoyle@gmail.com>
2019-12-28 15:29:11 +0100

    Manually set tahcyon URL

--- .config/load.php
+++ .config/load.php
@@ -1,3 +1,9 @@
 <?php
 
 define( 'AUTHBROKER_LOG', ini_get( 'error_log' ) );
+
+if ( Altis\get_environment_type() !== 'local' ) {
+	// wp-api.org/tachyon isn't accessible, because the main network site
+	// is not accessible.
+	define( 'TACHYON_URL', 'https://demo.wp-api.org/tachyon' );
+}
